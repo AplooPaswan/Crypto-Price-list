966b089d19a5a890dd47e0fb0534ae0ed90b688f
Aploo Paswan <aploopaswan2020@gmail.com>
2021-09-14 22:07:40 +0530

    google webmaster

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import Navbar from '../Components/Navbar'
 import AfterLoad from '../Components/AfterLoad'
 import Loader from '../Components/Skeleton1'
 import { extendTheme } from "@chakra-ui/react"
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
 
@@ -22,6 +23,9 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <div style={{display:''}}>
+      <Head>
+        <meta name="google-site-verification" content="IB2cU-2eG7MrUQfnVcXZiiU1vLntHBvCPPBSx3Htqvs" />
+      </Head>
       <ChakraProvider theme={theme}>
         <Navbar colorScheme="myColor"/>
         {/* <Header/> */}
